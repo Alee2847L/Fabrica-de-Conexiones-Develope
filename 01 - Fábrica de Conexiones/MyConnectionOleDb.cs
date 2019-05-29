1d32373677af862fb7222677216f094712dea2f0
Leonel Ramos <Leonel Ramos@DESKTOP-RV9MRO8>
2019-05-29 12:42:20 -0600

    La conexion OleDb

--- 01 - Fábrica de Conexiones/MyConnectionOleDb.cs
+++ 01 - Fábrica de Conexiones/MyConnectionOleDb.cs
@@ -1,6 +1,52 @@
-﻿namespace _01___Fábrica_de_Conexiones
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+
+namespace _01___Fábrica_de_Conexiones
 {
-    internal class MyConnectionOleDb
+    class MyConnectionOleDb : MyConnection
     {
+        // Constructor
+        public MyConnectionOleDb(DataProvider theDataProvider) : base(theDataProvider)
+        {
+            // Crear la cadena de conexión
+            OleDbConnection connectionString = new OleDbConnection(@"provider = sqloledb;
+                data source = (local)\sqlexpress; integrated security = sspi;");
+
+            try
+            {
+                // Establecer la conexión
+                connectionString.Open();
+                Console.WriteLine("Conexión establecida");
+
+                // Detalles de la conexión
+                this.DetallesConexion(connectionString);
+            }
+            catch (OleDbException e)
+            {
+                // Desplegar el error en la conexión
+                Console.WriteLine("Error: {0} {1}", e.Message, e.StackTrace);
+            }
+            finally
+            {
+                // Cerrar la conexión
+                connectionString.Close();
+            }
+        }
+
+        protected void DetallesConexion(OleDbConnection connectionString)
+        {
+            Console.WriteLine("Conexión establecida");
+            Console.WriteLine("\tConnection string: {0}", connectionString.ConnectionString);
+            Console.WriteLine("\tBase de datos: {0}", connectionString.Database);
+            Console.WriteLine("\tFuente de datos: {0}", connectionString.DataSource);
+            Console.WriteLine("\tVersión del servidor: {0}", connectionString.ServerVersion);
+            Console.WriteLine("\tEstado: {0}", connectionString.State);
+        }
     }
 }
